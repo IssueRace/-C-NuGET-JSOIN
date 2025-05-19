18d5f9521d3ebaf34c57955d222b2fe691557c68
Oleksandr <aleks2006dp@gmail.com>
2025-05-19 11:50:31 +0300

    Step 3: Added theoretical XML reader example

---
+++ XmlReaderExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+
+public class XmlReaderExample
+{
+    public static void Example()
+    {
+        using (XmlReader reader = XmlReader.Create("example.xml"))
+        {
+            while (reader.Read())
+            {
+                if (reader.IsStartElement())
+                {
+                    Console.WriteLine($"Element: {reader.Name}");
+                }
+            }
+        }
+    }
+}
